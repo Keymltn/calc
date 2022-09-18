f15dc5c01349cfdf83a204d1201ab41a4b0d8aeb
Keymltn <gulnozarahmatova21@gmail.com>
2022-09-18 11:41:11 +0500

    good???

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-let fir = document.querySelector("#fir").value
-let second = document.querySelector("#second").value
-let third = document.querySelector("#third").value
-let btn = document.querySelector("#btn").value
+let fir = document.querySelector("#fir")
+let second = document.querySelector("#second")
+let third = document.querySelector("#third")
+let btn = document.querySelector("#btn")
+let h12 = document.getElementById("h12")
 
-calc(fir, second, third, btn);
-
-function calc(f, s, t, b) {
-    btn.addEventListener("click" , () => {
-        console.log(eval = (+f + +s + t));
-    })
-}
+btn.addEventListener("click" , (e) => {
+    e.preventDefault()
+    h12.innerHTML = eval(fir.value + second.value + third.value);
+})
